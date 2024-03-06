34ad3a859b1c8f8d7be13bdfe4a6512f09a688ed
AoKuShi <161807984+AoKuShi@users.noreply.github.com>
2024-03-06 17:45:33 +0900

    Add files via upload

---
+++ chap02/src/sec04/Print01.java
@@ -0,0 +1,18 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		String myName = "홍길동";
+		 int myAge = 20;
+		 System.out.println("이름 : " + myName);
+		 System.out.println("나이 : " + myAge);
+		 System.out.println();
+//		 System.out.print("이름 : " + myName);
+//		 System.out.println("나이 : " + myAge);
+		 System.out.println("이름 : " + myName + " 이름 : " + myAge);
+		
+
+	}
+
+}
