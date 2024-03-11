6e4abe01a721d88e888cefd44485fd0c7a035267
AoKuShi <161807984+AoKuShi@users.noreply.github.com>
2024-03-11 13:34:52 +0900

    Add files via upload

---
+++ chap04/src/chap04/Example02.java
@@ -0,0 +1,13 @@
+package chap04;
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		System.out.println("* Java Program");
+		System.out.println("* Java\tProgram");
+		System.out.println("* Java\nProgram");
+		System.out.println("* \"Java Program\"");
+
+	}
+
+}
