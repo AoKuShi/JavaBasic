f19cf2555a5ec41c7a4d6a40257a1d002943f72b
AoKuShi <161807984+AoKuShi@users.noreply.github.com>
2024-06-16 18:51:36 +0900

    Add files via upload

---
+++ 240409_자바기초_과제/Exercise/bin/chap02/SchedulerTest/SwP.java
@@ -0,0 +1,17 @@
+package SchedulerTest;
+
+public class SwP {
+  public int PID;
+  public int eTime;
+  
+  public SwP(int PID, int eTime) {
+    this.PID = PID;
+    this.eTime = eTime;
+  }
+
+  // @Override
+  // public String toString() {
+  //   return "SwProcess[PID = " + PID + ", time = " + eTime + "]";
+  // }
+
+}
