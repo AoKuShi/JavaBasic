14b32c2c19b94c0f3cc0ba035cdcd02387f9ebcd
AoKuShi <161807984+AoKuShi@users.noreply.github.com>
2024-06-16 19:04:46 +0900

    Update Scheduler.java

--- 240409_자바기초_과제/Exercise/bin/chap02/SchedulerTest/Scheduler.java
+++ 240409_자바기초_과제/Exercise/bin/chap02/SchedulerTest/Scheduler.java
@@ -1,60 +1,59 @@
 package SchedulerTest;
 
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Scheduler {
-  public static void main(String[] args) {
-    Scheduler s =  new Scheduler();
+    public static void main(String[] args) {
+        Scheduler s = new Scheduler();
 
-    s.insert(new SwP(1, 7));
-    s.start();
+        s.insert(new SwP(1, 7));
+        s.start();
 
-    s.sleep(1000);
-    s.insert(new SwP(2, 3));
-    s.sleep(1000);
-    s.insert(new SwP(3, 1));
-
-  }
-
-  List<SwP> rQ = new CopyOnWriteArrayList<>();
-  ExecutorService rT = Executors.newSingleThreadExecutor();
-
-  void insert(SwP p){
-    rQ.add(p);
-  }
-
-  void sleep(int i) {
-    try {
-      TimeUnit.MILLISECONDS.sleep(i);
-    } catch (Exception e) {
-      e.getMessage();
+        s.sleep(1000);
+        s.insert(new SwP(2, 3));
+        s.sleep(1000);
+        s.insert(new SwP(3, 1));
     }
-  }
 
-  void log(SwP p) {
-    System.out.println(p + " is running " + rQ);
-  }
+    List<SwP> rQ = new CopyOnWriteArrayList<>();
+    ExecutorService rT = Executors.newSingleThreadExecutor();
 
-  void start() {
-    rT.execute(() -> {
-      while (rQ.size() != 0) {
-        for (SwP p : rQ) {
-          log(p);
-          sleep(1000);
-          p.eTime--;
+    void insert(SwP p) {
+        rQ.add(p);
+    }
 
-          if (p.eTime == 0) {
-            rQ.removeIf(j -> j.eTime == 0);
-          }
+    void sleep(int i) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(i);
+        } catch (Exception e) {
+            e.printStackTrace(); // 예외 처리 수정
         }
-      }
+    }
 
-      rT.shutdown();
-      System.exit(0);
-    });
-  }
+    void log(SwP p) {
+        System.out.println(p + " is running " + rQ);
+    }
 
+    void start() {
+        rT.execute(() -> {
+            while (rQ.size() != 0) {
+                for (SwP p : rQ) {
+                    log(p);
+                    sleep(1000);
+                    p.eTime--;
+
+                    if (p.eTime == 0) {
+                        rQ.removeIf(j -> j.eTime == 0);
+                    }
+                }
+            }
+
+            rT.shutdown();
+            System.exit(0);
+        });
+    }
 }
-
-
